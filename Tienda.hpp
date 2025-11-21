25e6316db03911c205240af442267701351cb6f3
3rick30090 <erickbu2007@gmail.com>
2025-11-20 20:59:37 -0600

    Add Inventario class and extend Tienda functionality

--- Tienda.hpp
+++ Tienda.hpp
@@ -56,10 +56,59 @@ public:
     float getTotal();
     void mostrarCarrito();
 };
-class Tienda {
+class Inventario {
+private:
+    Producto lista[50];
+    int numProductos = 0;
+
 public:
-    void procesarCompra(Carrito carrito, Comprador& comprador);
+    Inventario() {}
+
+    void agregarProducto(Producto p) {
+        if (numProductos < 50) {
+            lista[numProductos++] = p;
+        }
+    }
+
+    void mostrarInventario() {
+        std::cout << "\nInventario:\n";
+        for (int i = 0; i < numProductos; i++) {
+            lista[i].mostrarInfo();
+        }
+    }
 };
+class Tienda {
+private:
+    Producto productos[50];
+    int numProductos = 0;
 
+    Comprador compradores[50];
+    int numCompradores = 0;
+
+public:
+void procesarCompra(Carrito carrito, Comprador& comprador);
+void agregarProducto(Producto p) {
+    if (numProductos < 50) {
+         productos[numProductos++] = p;
+        }
+    }
+    void mostrarProductos() {
+        std::cout << "\nProductos en Tienda:\n";
+        for (int i = 0; i < numProductos; i++) {
+            productos[i].mostrarInfo();
+        }
+    }
+
+    void agregarComprador(Comprador c) {
+        if (numCompradores < 50) {
+            compradores[numCompradores++] = c;
+        }
+    }
+    void mostrarCompradores() {
+        std::cout << "\nCompradores registrados:\n";
+        for (int i = 0; i < numCompradores; i++) {
+            compradores[i].mostrarInfo();
+        }
+    }
+};
 #endif
-
