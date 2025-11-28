6066c06518fa58d116929cd0e7546f7cb33e5ddb
3rick30090 <erickbu2007@gmail.com>
2025-11-28 09:40:12 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main() {
     inventario.agregarProducto(Producto("God of War Ragnarok", 1499.0, 5));
     inventario.agregarProducto(Producto("Overwatch", 999.0, 20));
 
-    cout << "\n=== CATALOGO INICIAL ===\n";
+    cout << "\n= CATALOGO INICIAL =\n";
     inventario.mostrarInventario();
 
     Comprador comprador("Miguel", 5000);
@@ -21,19 +21,20 @@ int main() {
     carrito.agregar(Producto("Elden Ring", 1399.0f, 1));
     carrito.agregar(Producto("FIFA 24", 999.0f, 2));
 
-    cout << "\n=== CARRITO ===\n";
+    cout << "\n= CARRITO =\n";
     carrito.mostrarCarrito();
 
     Tienda tienda(inventario);
 
     tienda.agregarAlInventario(Producto("Spider-Man 2", 1499.0f, 12));
 
-    cout << "\n=== INVENTARIO ACTUALIZADO ===\n";
+    cout << "\n= INVENTARIO ACTUALIZADO =\n";
     tienda.mostrarInventario();
 
     tienda.procesarCompra(carrito, comprador);
 
-    cout << "\n=== FIN ===\n";
+    cout << "\n= FIN =\n";
 
     return 0;
 }
+
